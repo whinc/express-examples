a32d3abd0d7e6ac88ae586e5241e71c2615efaeb
whincwu <whincwu@163.com>
2023-04-22 12:29:40 +0800

    update nginx

--- nginx/curl.sh
+++ nginx/curl.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-# 模拟特定 server_name 发起请求
+# Host 指定请求的虚拟主机
 curl -v -H "Host: example.com" localhost:8080
+
+# Origin 指定跨域请求的来源
+curl -v -H "Host: example.com" -H "Origin: http://abc.com:3333" localhost:8080
+curl -v -H "Host: example.com" -H "Origin: http://example.com:9090" localhost:9090
