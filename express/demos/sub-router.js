b5049901fd005c716aacd0d40d061f1293e7aeb6
whincwu <whincwu@163.com>
2023-08-12 13:09:22 +0800

    add(pnpm): 增加nestjs依赖示例

--- express/demos/sub-router.js
+++ express/demos/sub-router.js
@@ -2,17 +2,41 @@ const express = require('express')
 
 const app = express()
 
+// -> app.handle
+// -> app._router.handle
+// -> find matched layer.path in stack(router) then call layer.handler
+// -> route.handler
+// -> find matched layer.method in stack(route) then call layer.handler
+//  -> user handler
 app.get('/foo', (req, res) => {
   res.send(`${req.method} ${req.url}`)
 })
+
 const blog = express.Router()
+
+// -> app.handle
+// -> app._router.handle
+// -> find matched layer.path in stack(router) then call layer.handler
+// -> sub router.handler
+// -> find matched layer.path in stack(sub router) then call layer.handler
+// -> user handler
 blog.use((req, res, next) => {
   res.appendHeader('X-middware', 1)
   next()
 })
 
+// -> app.handle
+// -> app._router.handle
+// -> find matched layer.path in stack(router) then call layer.handler
+// -> sub router.handler
+// -> find matched layer.path in stack(sub router) then call layer.handler
+// -> route.handler
+// -> find matched layer.method in stack(route) then call layer.handler
+// -> user handler
 blog.get('/zoo', (req, res) => {
   res.send(`${req.method} ${req.url}`)
 })
+
 app.use('/blog', blog)
+
 app.listen(3002)
